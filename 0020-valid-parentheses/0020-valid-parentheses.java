7b1e56f584a8e010fe5eee0cdeb38d5b533563f8
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-09 17:17:26 +0530

    Time: 3 ms (62.09%), Space: 43.4 MB (5.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,44 @@
 class Solution {
     public boolean isValid(String s) {
+        int length = s.length();
+
         Stack<Character> stack = new Stack<>();
 
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                stack.push(s.charAt(i));
-            } else if(!stack.isEmpty()) {
-                if(
-                    (stack.peek() == '(' && s.charAt(i) == ')') ||
-                    (stack.peek() == '{' && s.charAt(i) == '}') ||
-                    (stack.peek() == '[' && s.charAt(i) == ']')                    
-                    ) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
+        for(int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
             } else {
-                return false;
+                if(ch == ')') {
+                    if(stack.size() == 0 || stack.peek() != '(') {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+                if(ch == '}') {
+                    if(stack.size() == 0 || stack.peek() != '{') {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+                if(ch == ']') {
+                    if(stack.size() == 0 || stack.peek() != '[') {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
             }
         }
 
-        if(stack.isEmpty()) {
-            return true;
-        } else {
+        if(stack.size() > 0) {
             return false;
+        } else {
+            return true;
         }
+
+
     }
 }
