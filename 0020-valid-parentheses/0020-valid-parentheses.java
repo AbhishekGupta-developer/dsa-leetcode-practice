2ed8a1d5d116904fe101e943b010161c41171dc4
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-04-27 17:06:20 +0530

    Time: 4 ms (40.88%), Space: 43.5 MB (16.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length() % 2 != 0) {
+            return false;
+        }
+        
         Stack<Character> stack = new Stack<>();
 
         for(int i = 0; i < s.length(); i++) {
